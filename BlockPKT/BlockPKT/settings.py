3da3e37d62c2c93f4a755c3c353f03a70829dfb3
Peter K. Tarnue <tarnuepeterk@gmail.com>
2025-03-18 15:49:40 +0000

    update
    update

--- BlockPKT/BlockPKT/settings.py
+++ BlockPKT/BlockPKT/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blockApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'BlockPKT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- BlockPKT/blockApp/views.py
+++ BlockPKT/blockApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import *
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog-app/index.html')
+
+
+def readmore(request):
+    return request(request, 'blog-app/readmore.html')
